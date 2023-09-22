7c0a2b2291f04fa3abad47b711ce3d2758f6a9d7
Taranchuk <nfhfyxer@gmail.com>
2023-09-22 21:45:16 +0300

    removed biocodable hack code

--- 1.4/Source/GestaltEngine/Utils.cs
+++ 1.4/Source/GestaltEngine/Utils.cs
@@ -25,52 +25,6 @@ namespace GestaltEngine
             {
                 harmony.Patch(hook, new HarmonyMethod(typeof(Utils), nameof(Utils.ResetStaticData)));
             }
-
-
-            foreach (var li in AC_DefOf.RM_HackBiocodedThings.ingredients)
-            {
-                li.filter = new ThingFilterBiocodable();
-                var list = li.filter.thingDefs;
-                if (list is null)
-                {
-                    list = new List<ThingDef>();
-                    li.filter.thingDefs = list;
-                }
-                foreach (var thingDef in DefDatabase<ThingDef>.AllDefs.Where(x => x.comps != null && x.HasAssignableCompFrom(typeof(CompBiocodable))))
-                {
-                    li.filter.SetAllow(thingDef, true);
-                    list.Add(thingDef);
-                }
-            }
-            AC_DefOf.RM_HackBiocodedThings.fixedIngredientFilter = new ThingFilterBiocodable();
-            var list2 = AC_DefOf.RM_HackBiocodedThings.fixedIngredientFilter.thingDefs;
-            if (list2 is null)
-            {
-                list2 = new List<ThingDef>();
-                AC_DefOf.RM_HackBiocodedThings.fixedIngredientFilter.thingDefs = list2;
-            }
-
-            foreach (var thingDef in DefDatabase<ThingDef>.AllDefs.Where(x => x.comps != null 
-                && x.HasAssignableCompFrom(typeof(CompBiocodable))))
-            {
-                list2.Add(thingDef);
-                AC_DefOf.RM_HackBiocodedThings.fixedIngredientFilter.SetAllow(thingDef, true);
-            }
-
-            AC_DefOf.RM_HackBiocodedThings.defaultIngredientFilter = new ThingFilterBiocodable();
-            var list3 = AC_DefOf.RM_HackBiocodedThings.defaultIngredientFilter.thingDefs;
-            if (list3 is null)
-            {
-                list3 = new List<ThingDef>();
-                AC_DefOf.RM_HackBiocodedThings.defaultIngredientFilter.thingDefs = list3;
-            }
-
-            foreach (var thingDef in DefDatabase<ThingDef>.AllDefs.Where(x => x.comps != null 
-                && x.HasAssignableCompFrom(typeof(CompBiocodable))))
-            {
-                list3.Add(thingDef);
-                AC_DefOf.RM_HackBiocodedThings.defaultIngredientFilter.SetAllow(thingDef, true);
-            }
         }
 
         public static void ResetStaticData()
