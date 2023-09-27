105ffc18812b4d9ef9f5eaf44d0892a9dcd3e183
Taranchuk <nfhfyxer@gmail.com>
2023-09-27 14:59:02 +0300

    transferable and bill tab tweaks

---
+++ 1.4/Source/GestaltEngine/HarmonyPatches/TransferableUIUtility_DoCountAdjustInterfaceInternal_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace GestaltEngine
+{
+    [HarmonyPatch(typeof(TransferableUIUtility), "DoCountAdjustInterfaceInternal")]
+    public static class TransferableUIUtility_DoCountAdjustInterfaceInternal_Patch
+    {
+        public static bool Prefix(Rect rect, Transferable trad, int index, int min, int max, bool flash, bool readOnly)
+        {
+            if (trad.AnyThing is Pawn pawn && pawn.RaceProps.IsMechanoid && pawn.GetOverseer() is Pawn overseer && overseer.TryGetGestaltEngineInstead(out var comp))
+            {
+                GUI.color = Color.grey;
+                Widgets.Label(rect, "RM.CannotAddGestaltControlled".Translate());
+                GUI.color = Color.white;
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
