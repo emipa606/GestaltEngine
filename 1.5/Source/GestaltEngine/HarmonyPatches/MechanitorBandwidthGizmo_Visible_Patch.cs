29669565c7daca360db0cb57d0ca4b2f927d4b8e
Taranchuk <nfhfyxer@gmail.com>
2024-04-17 16:55:14 +0300

    1.5 fixes

---
+++ 1.5/Source/GestaltEngine/HarmonyPatches/MechanitorBandwidthGizmo_Visible_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace GestaltEngine
+{
+    [HarmonyPatch(typeof(MechanitorBandwidthGizmo), "Visible", MethodType.Getter)]
+    public static class MechanitorBandwidthGizmo_Visible_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            if (!__result)
+            {
+                __result = Find.Selector.SelectedObjects.OfType<Building>().Where(x => x.GetComp<CompGestaltEngine>() != null).Count() == 1;
+            }
+        }
+    }
+}
